86a4fc26bce41e661ab31b4fa89cd9a5938d4711
clixmods <clix.mods.cod@gmail.Com>
2024-01-03 21:33:36 +0100

    Intro video has a correct volume, earrape is gone now !

--- Assets/VideoHandler.cs
+++ Assets/VideoHandler.cs
@@ -1,27 +1,40 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Serialization;
 using UnityEngine.Video;
 
 public class VideoHandler : MonoBehaviour
 {
+    
+    #region Unity Hierarchy
+
     private VideoPlayer _videoPlayer;
+
+    #endregion
+    
     [SerializeField] private SettingsScriptableObject settingsScriptableObject;
+    
+    #region Unity Events
+
     public UnityEvent OnVideoStart;
+    
     public UnityEvent OnVideoStop;
+    
+    #endregion
+    
+    #region Init Methods
+
     private void Awake()
     {
         _videoPlayer = GetComponent<VideoPlayer>();
         enabled = false;
     }
+    
+    #endregion
 
-    // Start is called before the first frame update
+    #region Logic Methods
+
     public void StartVideo()
     {
-        _videoPlayer.SetDirectAudioVolume(0, Mathf.Clamp01( (settingsScriptableObject.VolumeGeneral+80f)/80f) );
         _videoPlayer.Play();
         OnVideoStart?.Invoke();
         enabled = true;
@@ -30,10 +43,15 @@ public class VideoHandler : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        // TODO : Warning if the videoPlayer has attribut WaitForFirstFrame true
+        // _videoPlayer.isPlaying will be false, and we don't want this behavior
         if (!_videoPlayer.isPlaying)
         {
             OnVideoStop?.Invoke();
             enabled = false;
         }
     }
+
+    #endregion
+    
 }
